880bb5f50322753cd5b4f4245835d5c9686a9b3e
Juan <jalvarado9000@gmail.com>
2022-02-12 19:00:21 -0800

    Submitting Week 4 Day 1 exercise

--- 05_Review_Day/exercise/src/main/java/com/techelevator/MakeChange.java
+++ 05_Review_Day/exercise/src/main/java/com/techelevator/MakeChange.java
@@ -1,4 +1,6 @@
 package com.techelevator;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
@@ -12,6 +14,31 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		boolean flag = false;
+		do {
+			System.out.println("Please enter the amount of the bill:");
+			double billAmount = input.nextDouble();
+			System.out.println("Please enter the amount tendered:");
+			double tenderAmount = input.nextDouble();
+			double changeRequired = tenderAmount - billAmount;
+
+			DecimalFormat f = new DecimalFormat("##.00");
+
+			if (billAmount > tenderAmount) {
+				System.out.println("Insufficient Amount please add $" + f.format(Math.abs(changeRequired))  + " to complete the payment");
+				System.out.println("Please try again");
+				flag = true;
+			}
+			else
+				System.out.println("The change required is " + f.format(changeRequired));
+
+		}while(flag);
+
+
+
+
+
 
 	}
 
